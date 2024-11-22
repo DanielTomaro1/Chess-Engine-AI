70f5a43ec32a87e3866152e91d31aa09eb21e954
DanielTomaro1 <danieltomaro@icloud.com>
2024-11-22 14:50:39 +1100

    Move evaluation

---
+++ evaluate.py
@@ -0,0 +1,251 @@
+##############################################################################################
+# Evaluation
+##############################################################################################
+import chess
+##############################################################################################
+# Piece Value Worth and Calculation
+##############################################################################################
+piece_values = {
+    chess.PAWN: 100,
+    chess.ROOK: 500,
+    chess.KNIGHT: 320,
+    chess.BISHOP: 330,
+    chess.QUEEN: 900,
+    chess.KING: 20000
+}
+
+board = chess.Board(chess.STARTING_FEN)
+white_material = 0
+black_material = 0
+
+for square in chess.SQUARES:
+    piece = board.piece_at(square)
+    if not piece:
+        continue
+    if piece.color == chess.WHITE:
+        white_material += piece_values[piece.piece_type]
+    else:
+        black_material += piece_values[piece.piece_type]
+##############################################################################################
+# Pawn
+##############################################################################################
+pawnEvalWhite = [
+    0,  0,  0,  0,  0,  0,  0,  0,
+    5, 10, 10, -20, -20, 10, 10,  5,
+    5, -5, -10,  0,  0, -10, -5,  5,
+    0,  0,  0, 20, 20,  0,  0,  0,
+    5,  5, 10, 25, 25, 10,  5,  5,
+    10, 10, 20, 30, 30, 20, 10, 10,
+    50, 50, 50, 50, 50, 50, 50, 50,
+    0, 0, 0, 0, 0, 0, 0, 0
+]
+pawnEvalBlack = list(reversed(pawnEvalWhite))
+##############################################################################################
+# Knight
+##############################################################################################
+knightEval = [
+    -50, -40, -30, -30, -30, -30, -40, -50,
+    -40, -20, 0, 0, 0, 0, -20, -40,
+    -30, 0, 10, 15, 15, 10, 0, -30,
+    -30, 5, 15, 20, 20, 15, 5, -30,
+    -30, 0, 15, 20, 20, 15, 0, -30,
+    -30, 5, 10, 15, 15, 10, 5, -30,
+    -40, -20, 0, 5, 5, 0, -20, -40,
+    -50, -40, -30, -30, -30, -30, -40, -50
+]
+##############################################################################################
+# Bishop
+##############################################################################################
+bishopEvalWhite = [
+    -20, -10, -10, -10, -10, -10, -10, -20,
+    -10, 5, 0, 0, 0, 0, 5, -10,
+    -10, 10, 10, 10, 10, 10, 10, -10,
+    -10, 0, 10, 10, 10, 10, 0, -10,
+    -10, 5, 5, 10, 10, 5, 5, -10,
+    -10, 0, 5, 10, 10, 5, 0, -10,
+    -10, 0, 0, 0, 0, 0, 0, -10,
+    -20, -10, -10, -10, -10, -10, -10, -20
+]
+bishopEvalBlack = list(reversed(bishopEvalWhite))
+##############################################################################################
+# Rook
+##############################################################################################
+rookEvalWhite = [
+    0, 0, 0, 5, 5, 0, 0, 0,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    5, 10, 10, 10, 10, 10, 10, 5,
+    0, 0, 0, 0, 0, 0, 0, 0
+]
+rookEvalBlack = list(reversed(rookEvalWhite))
+##############################################################################################
+# Queen
+##############################################################################################
+queenEval = [
+    -20, -10, -10, -5, -5, -10, -10, -20,
+    -10, 0, 0, 0, 0, 0, 0, -10,
+    -10, 0, 5, 5, 5, 5, 0, -10,
+    -5, 0, 5, 5, 5, 5, 0, -5,
+    0, 0, 5, 5, 5, 5, 0, -5,
+    -10, 5, 5, 5, 5, 5, 0, -10,
+    -10, 0, 5, 0, 0, 0, 0, -10,
+    -20, -10, -10, -5, -5, -10, -10, -20
+]
+##############################################################################################
+# King not End Game
+##############################################################################################
+kingEvalWhite = [
+    20, 30, 10, 0, 0, 10, 30, 20,
+    20, 20, 0, 0, 0, 0, 20, 20,
+    -10, -20, -20, -20, -20, -20, -20, -10,
+    20, -30, -30, -40, -40, -30, -30, -20,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30
+]
+kingEvalBlack = list(reversed(kingEvalWhite))
+##############################################################################################
+# King End Game
+##############################################################################################
+kingEvalEndGameWhite = [
+    50, -30, -30, -30, -30, -30, -30, -50,
+    -30, -30,  0,  0,  0,  0, -30, -30,
+    -30, -10, 20, 30, 30, 20, -10, -30,
+    -30, -10, 30, 40, 40, 30, -10, -30,
+    -30, -10, 30, 40, 40, 30, -10, -30,
+    -30, -10, 20, 30, 30, 20, -10, -30,
+    -30, -20, -10,  0,  0, -10, -20, -30,
+    -50, -40, -30, -20, -20, -30, -40, -50
+]
+kingEvalEndGameBlack = list(reversed(kingEvalEndGameWhite))
+# fmt: on
+##############################################################################################
+# This is move ordering here and is basically saying priortize promotion then weaker taking stronger etc ....
+##############################################################################################
+def move_value(board: chess.Board, move: chess.Move, endgame: bool) -> float:
+    """
+    How good is a move?
+    A promotion is great.
+    A weaker piece taking a stronger piece is good.
+    A stronger piece taking a weaker piece is bad.
+    Also consider the position change via piece-square table.
+    """
+    if move.promotion is not None:
+        return -float("inf") if board.turn == chess.BLACK else float("inf")
+
+    _piece = board.piece_at(move.from_square)
+    if _piece:
+        _from_value = evaluate_piece(_piece, move.from_square, endgame)
+        _to_value = evaluate_piece(_piece, move.to_square, endgame)
+        position_change = _to_value - _from_value
+    else:
+        raise Exception(f"A piece was expected at {move.from_square}")
+
+    capture_value = 0.0
+    if board.is_capture(move):
+        capture_value = evaluate_capture(board, move)
+
+    current_move_value = capture_value + position_change
+    if board.turn == chess.BLACK:
+        current_move_value = -current_move_value
+
+    return current_move_value
+##############################################################################################
+# This is evaluating how good a capture is 
+##############################################################################################
+def evaluate_capture(board: chess.Board, move: chess.Move) -> float:
+    """
+    Given a capturing move, weight the trade being made.
+    """
+    if board.is_en_passant(move):
+        return piece_values[chess.PAWN]
+    _to = board.piece_at(move.to_square)
+    _from = board.piece_at(move.from_square)
+    if _to is None or _from is None:
+        raise Exception(
+            f"Pieces were expected at _both_ {move.to_square} and {move.from_square}"
+        )
+    return piece_values[_to.piece_type] - piece_values[_from.piece_type]
+##############################################################################################
+# It computes a positional score for a given piece on a specific square based on piece-square tables, considering the game's phase (endgame or not)
+# It uses the positional boards to determine how good a move is
+##############################################################################################
+def evaluate_piece(piece: chess.Piece, square: chess.Square, end_game: bool) -> int:
+    piece_type = piece.piece_type
+    mapping = []
+    if piece_type == chess.PAWN:
+        mapping = pawnEvalWhite if piece.color == chess.WHITE else pawnEvalBlack
+    if piece_type == chess.KNIGHT:
+        mapping = knightEval
+    if piece_type == chess.BISHOP:
+        mapping = bishopEvalWhite if piece.color == chess.WHITE else bishopEvalBlack
+    if piece_type == chess.ROOK:
+        mapping = rookEvalWhite if piece.color == chess.WHITE else rookEvalBlack
+    if piece_type == chess.QUEEN:
+        mapping = queenEval
+    if piece_type == chess.KING:
+        # use end game piece-square tables if neither side has a queen
+        if end_game:
+            mapping = (
+                kingEvalEndGameWhite
+                if piece.color == chess.WHITE
+                else kingEvalEndGameBlack
+            )
+        else:
+            mapping = kingEvalWhite if piece.color == chess.WHITE else kingEvalBlack
+
+    return mapping[square]
+##############################################################################################
+# Evaluates the board
+##############################################################################################
+def evaluate_board(board: chess.Board) -> float:
+    """
+    Evaluates the full board and determines which player is in a most favorable position.
+    The sign indicates the side:
+        (+) for white
+        (-) for black
+    The magnitude, how big of an advantage that player has
+    """
+    total = 0
+    end_game = check_end_game(board)
+
+    for square in chess.SQUARES:
+        piece = board.piece_at(square)
+        if not piece:
+            continue
+
+        value = piece_values[piece.piece_type] + evaluate_piece(piece, square, end_game)
+        total += value if piece.color == chess.WHITE else -value
+
+    return total
+##############################################################################################
+# End game check
+##############################################################################################
+def check_end_game(board: chess.Board) -> bool:
+    """
+    Are we in the end game?
+    Per Michniewski:
+    - Both sides have no queens or
+    - Every side which has a queen has additionally no other pieces or one minorpiece maximum.
+    """
+    queens = 0
+    minors = 0
+
+    for square in chess.SQUARES:
+        piece = board.piece_at(square)
+        if piece and piece.piece_type == chess.QUEEN:
+            queens += 1
+        if piece and (
+            piece.piece_type == chess.BISHOP or piece.piece_type == chess.KNIGHT
+        ):
+            minors += 1
+
+    if queens == 0 or (queens == 2 and minors <= 1):
+        return True
+
+    return False
+##############################################################################################
